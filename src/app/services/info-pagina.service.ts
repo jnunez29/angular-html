33c9516ded2eb7de8c097de4fefda26f8bde4a5b
Jorge Nuñez <jorgenunezsolis@gmail.com>
2019-10-05 13:20:07 -0500

    fin de la seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -7,18 +7,28 @@ import { InfoPagina } from '../interfaces/infor-pagina-interface';
 })
 export class InfoPaginaService {
   info: InfoPagina = {};
+  equipo: any;
   cargada: boolean = false;
   constructor(private http: HttpClient) {
-    // leer archivo json
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe((resp: InfoPagina) => {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-      })
+   this.cargarInfo();
+   this.cargarEquipo();
   }
 
+private cargarInfo(){
+ // leer archivo json
+ this.http.get('assets/data/data-pagina.json')
+ .subscribe((resp: InfoPagina) => {
+   this.cargada = true;
+   this.info = resp;
+ })
+}
 
-
-
+private cargarEquipo(){
+  this.http.get('https://angular-html-16bd6.firebaseio.com/equipo.json')
+ .subscribe((resp: any[]) => {
+   this.cargada = true;
+   this.equipo = resp;
+   console.log(this.equipo);
+ })
+}
 }
